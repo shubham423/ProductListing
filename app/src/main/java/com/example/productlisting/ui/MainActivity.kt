b5ff89b9fc0405d397ad3f48827ddfe0a19589c8
shubham423 <shubh991172@gmail.com>
2023-07-25 22:02:26 +0530

    setup navigation component libaray

--- app/src/main/java/com/example/productlisting/ui/MainActivity.kt
+++ app/src/main/java/com/example/productlisting/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.productlisting.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.productlisting.R
+import com.example.productlisting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
